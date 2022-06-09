efa39ecc28c15229794be98a6091bf8243704ee6
Leonieke <datwatikmaak@gmail.com>
2022-06-09 09:46:06 +0200

    submission Bite 43 @ codechalleng.es

--- 43/kwargs.py
+++ 43/kwargs.py
@@ -1,1 +1,7 @@
-def get_profile(...
+def get_profile(*, name="julian", profession="programmer"):
+    return f"{name} is a {profession}"
+
+
+kwargs = {"name": "julian", "profession": "programmer"}
+
+print(get_profile(**kwargs))
