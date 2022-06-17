bbd571fe943c8ecd54e8a32e9420e0fb0318b288
Leonieke <datwatikmaak@gmail.com>
2022-06-17 08:28:09 +0200

    submission Bite 66 @ codechalleng.es

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -1,5 +1,12 @@
+import itertools
+
+
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
        returns a sequence of same length with the averages.
        You can assume all items in sequence are numeric."""
-    pass
+    averages = list(itertools.starmap(
+        lambda a, b: b / a,
+        enumerate(itertools.accumulate(sequence), 1))
+    )
+    return [round(num, 2) for num in averages]
