4db1f77c4d1e1e79b6fe92b2d1663ddf5c248757
Leonieke <datwatikmaak@gmail.com>
2022-06-23 09:19:22 +0200

    submission Bite 91 @ codechalleng.es

--- 91/anyall.py
+++ 91/anyall.py
@@ -4,17 +4,17 @@ PYTHON = 'python'
 
 def contains_only_vowels(input_str):
     """Receives input string and checks if all chars are
-       VOWELS. Match is case insensitive."""
-    pass
+       VOWELS. Match is case-insensitive."""
+    return all(char.lower() in VOWELS for char in input_str.lower())
 
 
 def contains_any_py_chars(input_str):
     """Receives input string and checks if any of the PYTHON
-       chars are in it. Match is case insensitive."""
-    pass
+       chars are in it. Match is case-insensitive."""
+    return any(char.lower() in input_str.lower() for char in PYTHON)
 
 
 def contains_digits(input_str):
     """Receives input string and checks if it contains
        one or more digits."""
-    pass
+    return any(char.isdigit() for char in input_str)
