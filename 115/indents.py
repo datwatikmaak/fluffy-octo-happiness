8e71994b35cef3feeca616e0c3afa76a96bfd6dc
Leonieke <datwatikmaak@gmail.com>
2022-06-26 04:50:01 +0200

    submission Bite 115 @ codechalleng.es

--- 115/indents.py
+++ 115/indents.py
@@ -2,4 +2,4 @@ def count_indents(text: str) -> int:
     """
     Count and return the number of leading white space characters (' ').
     """
-    pass
+    return len(text) - len(text.lstrip(' '))
