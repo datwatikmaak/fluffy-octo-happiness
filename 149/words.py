fe3cb45cb0cc71f1c8557d36ddc5c5f7aff226f0
Leonieke <datwatikmaak@gmail.com>
2022-06-30 20:04:48 +0200

    submission Bite 149 @ codechalleng.es

--- 149/words.py
+++ 149/words.py
@@ -3,4 +3,16 @@ def sort_words_case_insensitively(words):
        (1995, 19ab = numbers / Happy, happy4you = strings, hence for
         numbers you only need to check the first char of the word)
     """
-    pass
+    numerics = []
+    alphabets = []
+   
+    for word in words:
+
+        # checking and inserting in respective container
+        if word[0].isdigit():
+            numerics.append(word)
+        else:
+            alphabets.append(word)
+
+    # attaching lists post sort
+    return sorted(alphabets, key=str.casefold) + sorted(numerics)
