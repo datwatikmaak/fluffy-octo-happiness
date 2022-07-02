9a6f2b0f12f89d3796bfd4105ca1751a6c807472
Leonieke <datwatikmaak@gmail.com>
2022-07-02 07:16:25 +0200

    submission Bite 161 @ codechalleng.es

--- 161/tree.py
+++ 161/tree.py
@@ -2,7 +2,13 @@ import os
 
 
 def count_dirs_and_files(directory='.'):
-    """Count the amount of of directories and files in passed in "directory" arg.
+    """Count the amount of directories and files passed in "directory" arg.
        Return a tuple of (number_of_directories, number_of_files)
     """
-    pass
+    files = folders = 0
+
+    for _, dirnames, filenames in os.walk(directory):
+        files += len(filenames)
+        folders += len(dirnames)
+
+    return folders, files
