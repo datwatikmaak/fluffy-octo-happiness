c2beafaa405f1e1bf4193711a5e9b669bf015daf
Leonieke <datwatikmaak@gmail.com>
2022-07-05 07:39:11 +0200

    submission Bite 172 @ codechalleng.es

--- 172/curry.py
+++ 172/curry.py
@@ -1,7 +1,12 @@
 from functools import partial
 
+
+def rounding(n, r):
+    return round(r, n)
+
+
 # create 2 partials:
 # - 'rounder_int' rounds to int (0 places)
 # - 'rounder_detailed' rounds to 4 places
-rounder_int =  # you code
-rounder_detailed =  # you code
+rounder_int = partial(rounding, 0)
+rounder_detailed = partial(rounding, 4)
