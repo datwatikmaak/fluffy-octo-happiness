ad9940f631439042f17fccc8aa88da78b008bb6f
Leonieke <datwatikmaak@gmail.com>
2022-07-06 06:46:59 +0200

    submission Bite 176 @ codechalleng.es

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -4,4 +4,18 @@ WHITE, BLACK = ' ', '#'
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
        Don't return anything, print the output to stdout"""
-    pass
+    even_rows = ""
+    odd_rows = ""
+    for i in range(size):
+        if i % 2 == 0:
+            even_rows += WHITE
+            odd_rows += BLACK
+        else:
+            even_rows += BLACK
+            odd_rows += WHITE
+
+    for i in range(size):
+        if i % 2 == 0:
+            print(even_rows)
+        else:
+            print(odd_rows)
