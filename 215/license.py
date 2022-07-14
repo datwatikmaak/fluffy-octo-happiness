48bdcb77ca3fd51a2925770efa19cb770329cb25
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-07-14 10:32:19 +0200

    Update license.py
    Delete commented line of code

--- 215/license.py
+++ 215/license.py
@@ -5,6 +5,5 @@ def validate_license(key: str) -> bool:
     """Write a regex that matches a PyBites license key
        (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
     """
-    # pattern = re.compile("^PB(?:-([A-Z]+|\d+){8}){4}$")
     pattern = re.compile("^PB((-[A-Z0-9]{8}){4})$")
     return bool(re.search(pattern, key))
