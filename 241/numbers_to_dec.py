07b42880fdffbff17c34ae538796924a9362b3b5
Leonieke <datwatikmaak@gmail.com>
2022-07-17 19:17:34 +0200

    submission Bite 241 @ codechalleng.es

--- 241/numbers_to_dec.py
+++ 241/numbers_to_dec.py
@@ -3,7 +3,7 @@ from typing import List
 
 def list_to_decimal(nums: List[int]) -> int:
     """Accept a list of positive integers in the range(0, 10)
-       and return a integer where each int of the given list represents
+       and return an integer where each int of the given list represents
        decimal place values from first element to last.  E.g
         [1,7,5] => 175
         [0,3,1,2] => 312
