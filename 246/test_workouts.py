afd4003adf8caf011225ee15adedcdb37827bfe9
Leonieke <datwatikmaak@gmail.com>
2022-07-19 05:04:29 +0200

    submission Bite 246 @ codechalleng.es

--- 246/test_workouts.py
+++ 246/test_workouts.py
@@ -3,5 +3,13 @@ import pytest
 from workouts import print_workout_days
 
 
-def test_print_workout_days():
-    pass
+@pytest.mark.parametrize('test_input, expected', [
+    ('upper', 'Mon, Thu'),
+    ('lower', 'Tue, Fri'),
+    ('cardio', 'Wed'),
+    ('running', 'No matching workout')
+])
+def test_print_workout_days(capsys, test_input, expected):
+    print_workout_days(test_input)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected
