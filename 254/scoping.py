b5e87803b5cf9239e51402a8bd01aa5beb343a7c
Leonieke <datwatikmaak@gmail.com>
2022-07-23 06:17:11 +0200

    submission Bite 254 @ codechalleng.es

--- 254/scoping.py
+++ 254/scoping.py
@@ -1,3 +1,5 @@
+from math import floor
+
 num_hundreds = -1
 
 
@@ -5,4 +7,11 @@ def sum_numbers(numbers: list) -> int:
     """Sums passed in numbers returning the total, also
        update the global variable num_hundreds with the amount
        of times 100 fits in total"""
-    pass
+    global num_hundreds
+
+    if not numbers or sum(numbers) < 100:
+        num_hundreds = num_hundreds
+    else:
+        num_hundreds += floor(sum(numbers) / 100)
+
+    return sum(numbers)
