1491557032a9209807e7c899e1b4dd8efb8dec10
Leonieke <datwatikmaak@gmail.com>
2022-07-24 04:05:18 +0200

    submission Bite 257 @ codechalleng.es

--- 257/users.py
+++ 257/users.py
@@ -5,4 +5,16 @@ def get_users(passwd: str) -> dict:
       replace multiple commas in name with a single space
       return dict of keys = user, values = name.
     """
-    pass
+    passwords = (list(filter(None, passwd.splitlines())))
+    words = [word.split(":") for word in passwords]
+
+    user = []
+    name = []
+    for word in words:
+        user.append(word[0])
+        if word[4] == "":
+            name.append("unknown")
+        else:
+            name.append(word[4].replace(",,,", "").replace(",", " "))
+
+    return dict(zip(user, name))
